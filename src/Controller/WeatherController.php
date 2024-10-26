784ca8e3ef372c22d4d74534e959663bcf83ba43
Daria R <skrzynkad@interia.pl>
2024-10-26 15:28:06 +0200

    Lab C

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,20 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Repository\LocationRepository;
+use App\Repository\MeteoDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+//#[Route('/weather/')]
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{id}', name: 'app_weather', requirements: ['id' => '\d+'])]
-    public function city(Location $location, MeteoDataRepository $repository): Response
+    //country? znaczy opcjonalnie
+    #[Route('/weather/{country}/{city}', name: 'app_weather')]
+    public function city(
+        LocationRepository $locationRepository,
+        MeteoDataRepository $meteoDataRepository,
+        string $city,
+        string $country = null
+    ): Response
     {
-        $measurements = $repository->findByLocation($location);
+        $location = $locationRepository->findOneBy([
+            'country' => $country,
+            'city' => $city,
+        ]);
+
+        $meteoData = $meteoDataRepository->findByLocation($location);
         return $this->render('weather/city.html.twig', [
             //'controller_name' => 'WeatherController',
             'location' => $location,
-            'measurements' => $measurements,
+            'meteoData' => $meteoData,
         ]);
     }
+
+    /*#[Route('/weather/highlander-says')]
+    public function highlanderSays(): Response
+    {
+        //int from 0 to 10
+        $draw = random_int(0,100);
+
+        $forecast = $draw < 50 ? "It's going to rain" : "It's going to be sunny";
+
+        //return response
+        return new Response(
+            "<html><body>$forecast</body></html>"
+        );
+
+        //better response
+        return $this->>render('weather/highlander_says.html.twig',[
+            'forecast' => $forecast,
+        ]);
+    }*/
 }
--- src/Repository/MeteoDataRepository.php
+++ src/Repository/MeteoDataRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Location;
 use App\Entity\MeteoData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,31 +17,6 @@ class MeteoDataRepository extends ServiceEntityRepository
         parent::__construct($registry, MeteoData::class);
     }
 
-    //    /**
-    //     * @return MeteoData[] Returns an array of MeteoData objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?MeteoData
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
-
     //labC - repozytorium
     public function findByLocation(Location $location)
     {
@@ -51,7 +27,6 @@ class MeteoDataRepository extends ServiceEntityRepository
             ->setParameter('now', date('Y-m-d'));
 
         $query = $qb->getQuery();
-        $result = $query->getResult();
-        return $result;
+        return $query->getResult();
     }
 }
